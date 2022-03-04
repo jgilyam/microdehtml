126beac51b4074cddeae096377dfb428898b0755
jgilyam <jpgilyam@gmail.com>
2022-03-03 22:22:55 -0300

    primer html con servidor estatico

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const res = require("express/lib/response");
+
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en pueto ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
