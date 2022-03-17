1930ef75e6e66aff9e67fa6e00538044f749551a
jgilyam <jpgilyam@gmail.com>
2022-03-16 21:48:22 -0300

    se construyo un formulario

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+  res.sendFile(path.resolve(__dirname, "./views/home2.html"));
 });
